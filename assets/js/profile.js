2cc5193b78b9f23c6a610d2e33d6e9d795a4c074
Ranvir Singh <ranvirsinghsodhi08@gmail.com>
2025-09-13 19:48:12 +0530

    made everything responsive and integrated all pages

--- assets/js/profile.js
+++
@@ -1,8 +0,0 @@
-// Sidebar active state toggle
-const sidebarItems = document.querySelectorAll(".sidebar li");
-sidebarItems.forEach(item => {
-  item.addEventListener("click", () => {
-    sidebarItems.forEach(i => i.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
